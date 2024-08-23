ac4c3c9bb650fc19d3731e9bf6254b9630753a60
Ravindra <sirviravindra609@gmail.com>
2024-08-23 12:57:29 +0530

    feat: Add header and footer components to layout
    The code changes add the `Header` and `Footer` components to the `layout.tsx` file. This ensures that the header and footer are displayed on all pages of the website.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,17 @@
-import Head from "next/head";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import Hero from "../components/Hero";
-import FeaturedSpeakers from "../components/FeaturedSpeakers";
-import KeyInfo from "../components/KeyInfo";
-import LatestNews from "../components/LatestNews";
+import Hero from "@/components/Hero";
 import AboutSection from "@/components/HomeAbout";
+import FeaturedSpeakers from "@/components/FeaturedSpeakers";
+import KeyInfo from "@/components/KeyInfo";
+import LatestNews from "@/components/LatestNews";
 
-const Home: React.FC = () => {
+export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Head>
-        <title>Global Bio Pharma Conference 2024</title>
-        <meta
-          name="description"
-          content="Join the leading Bio Pharma Conference of 2024. Network with industry experts, discover breakthrough research, and shape the future of healthcare."
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Header />
-
-      <main className="flex-grow">
-        <Hero />
-        <AboutSection />
-        <FeaturedSpeakers />
-        <KeyInfo />
-        <LatestNews />
-      </main>
-
-      <Footer />
-    </div>
+    <>
+      <Hero />
+      <AboutSection />
+      <FeaturedSpeakers />
+      <KeyInfo />
+      <LatestNews />
+    </>
   );
-};
-
-export default Home;
+}
